d97c4dcebe887304d62f2d3a56fa54c26351cfa1
ousaka99 <ousaka99@gmail.com>
2018-08-18 20:44:34 +0900

    ランク表示改修
    ダメージ降順、日時昇順、ID順
    その他改修

--- bot_config.py
+++ bot_config.py
@@ -8,10 +8,15 @@ class BotConfig:
         self.config.read('./setting.conf', encoding="utf-8_sig")
         self.bot_token = self.config['default']['bot_token']
         self.channel_id = self.config['default']['channel_id']
-        self.command_set_ign = self.config['default']['command_set_ign'].split()
+        self.command_help = self.config['default']['command_help']
+        self.command_set_ign = self.config['default']['command_set_ign']
         self.command_kuji = self.config['default']['command_kuji'].split()
-        self.kuji_first_day = datetime.datetime.strptime(self.config['default']['kuji_first_day'], '%Y/%m/%d')
-        self.kuji_last_day = datetime.datetime.strptime(self.config['default']['kuji_last_day'], '%Y/%m/%d')
+        self.command_previous_rank = self.config['default']['command_previous_rank']
+        self.command_rank = self.config['default']['command_rank']
+        d = datetime.datetime.strptime(self.config['default']['kuji_first_day'], '%Y/%m/%d')
+        self.kuji_first_day = datetime.date(d.year, d.month, d.day)
+        d = datetime.datetime.strptime(self.config['default']['kuji_last_day'], '%Y/%m/%d')
+        self.kuji_last_day = datetime.date(d.year, d.month, d.day)
         self.kuji_days = []
         if self.kuji_first_day <= self.kuji_last_day:
             d = self.kuji_first_day
@@ -19,3 +24,4 @@ class BotConfig:
             while d <= self.kuji_last_day:
                 self.kuji_days.append(d)
                 d += delta
+        self.rank_count = 10
